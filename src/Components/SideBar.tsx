4d08db1119ffd906ad76767295b1e0de4f8e9766
SimonSCC <simonsondergaardchristiansen@gmail.com>
2022-03-24 01:04:09 +0100

    Rewriting frontend code to typescript

---
+++ src/Components/SideBar.tsx
@@ -0,0 +1,20 @@
+
+import { FaGlassMartiniAlt } from "react-icons/fa";
+import { GiSchoolBag } from "react-icons/gi";
+import { FaBook } from "react-icons/fa";
+import SideBarIcon from "./SideBarIcon";
+
+const SideBar = () => {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-48 m-0 flex flex-col  text-foreground shadow-lg bg-gradient-to-b from-color8/10 via-color1 to-color8/5 rounded-lg">
+            <SideBarIcon icon={<FaGlassMartiniAlt size="44" />} path="whatcanimake" tooltip="What can I make" />
+            <SideBarIcon icon={<GiSchoolBag size="52" />} path="myingredients" tooltip="My ingredients" />
+            <SideBarIcon icon={<FaBook size="44" />} path="allrecipes" tooltip="All recipes" />
+        </div>
+    );
+};
+
+export default SideBar;
+
+
+
